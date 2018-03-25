930a79cbd19f874eee669fed550f8c3bc90491d4
David Ritchey <ritchey0713@gmail.com>
2018-03-25 06:00:08 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -8,7 +8,8 @@ end
 def shipping_manifest
   the_manifest => {}
   the_manifest = ["whale bone corsets"] = 5
-  the_manifest = ["porcelain vases"] = 2
+  the_manifest = ["porcelain vases"] = 2 
+  the_manifest = ["oil paintings"] = 3
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
 
 
