7f5e6eea3290a1bf5e81db918c605b80aa403e61
David Ritchey <ritchey0713@gmail.com>
2018-03-25 06:01:41 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -7,10 +7,10 @@ end
 
 def shipping_manifest
   the_manifest => {}
-  the_manifest = ["whale bone corsets"] = 5
-  the_manifest = ["porcelain vases"] = 2 
-  the_manifest = ["oil paintings"] = 3
- 
+  the_manifest["whale bone corsets"] = 5
+  the_manifest["porcelain vases"] = 2 
+  the_manifest["oil paintings"] = 3
+
 
 
 end
